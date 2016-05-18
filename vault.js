c667c50142e585d7bcd55544890c398102dc527d
Nikki K <808nikz@gmail.com>
2016-05-17 21:24:15 -1000

    Got a secret to return null

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
   
+  var secret = null,
+   	result = {};
+
+  function setValue(key,value){
+  	
+  }
+
+  return{
+  	getValue: function(key){
+  		return secret;
+  	}
+  };
+  // function setValue(key,value){
+  // 	if(key === value){
+  // 		return key;
+  // 	}else{
+  // 		return function getValue(key){
+  // 			return secrets;
+  // 		};
+  // 	}
+  // }
+	
 };
